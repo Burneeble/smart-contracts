f228841e910320f8b81451c8fd8774f125d73a94
Alessandro privitera <63925908+DogeBao@users.noreply.github.com>
2023-08-14 06:44:02 +0200

    init repo

---
+++ hardhat.config.ts
@@ -0,0 +1,19 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+};
+
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.WALLET_PRIVATE_KEY]
+    }
+  }
+};
